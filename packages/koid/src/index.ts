e58a0294f06d06e7fafb9739ea9f2f1498abc682
waiting <1661926154@qq.com>
2021-01-16 23:16:28 +0800

    chore(koid): export type IdInfo

--- packages/koid/src/index.ts
+++ packages/koid/src/index.ts
@@ -2,7 +2,7 @@
 export { KoidFactory } from './lib/index'
 export type { Koid } from './lib/koid'
 export {
-  ConfigId, ConfigDc, KoidMsg,
+  ConfigId, ConfigDc, KoidMsg, IdInfo,
 } from './lib/types'
 export { genConfigRandom } from './lib/helper'
 export { retrieveFromId } from './lib/util'
