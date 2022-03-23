2b22dc4fce4f88531617d4bdc6c5d8417297ea74
waiting <1661926154@qq.com>
2022-03-23 17:37:55 +0800

    chore(midway-component-koid): move type merging into src/index.ts

--- packages/midway-component-koid/src/index.ts
+++ packages/midway-component-koid/src/index.ts
@@ -1,12 +1,24 @@
+import {
+  Config,
+  ConfigKey,
+  MiddlewareConfig,
+} from './lib/index'
+
 
 export { AutoConfiguration as Configuration } from './configuration'
 export * from './lib/index'
-
-
 export {
   ConfigDc,
   ConfigNode,
   KoidMsg,
   genConfigRandom,
 } from 'koid'
+
+
+declare module '@midwayjs/core' {
+  interface MidwayConfig {
+    [ConfigKey.config]: Config
+    [ConfigKey.middlewareConfig]: MiddlewareConfig
+  }
+}
 
