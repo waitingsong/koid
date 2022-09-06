a87eafde3f2b8dfa32e1b96baca840b2dc8d070c
waiting <1661926154@qq.com>
2022-09-06 13:48:08 +0800

    ci: 8

--- packages/midway-component-koid/src/configuration.ts
+++ packages/midway-component-koid/src/configuration.ts
@@ -4,17 +4,15 @@ import { join } from 'node:path'
 
 import { ILifeCycle } from '@midwayjs/core'
 import { Configuration } from '@midwayjs/decorator'
-import * as koa from '@midwayjs/koa'
 
+import { useComponents } from './components'
 import { ConfigKey } from './lib/types'
 
 
 @Configuration({
   namespace: ConfigKey.namespace,
   importConfigs: [join(__dirname, 'config')],
-  imports: [
-    koa,
-  ],
+  imports: useComponents,
 })
 export class AutoConfiguration implements ILifeCycle {
 }
--- packages/midway-component-koid/src/middleware/index.middleware.ts
+++ packages/midway-component-koid/src/middleware/index.middleware.ts
@@ -1,5 +1,5 @@
 
 export * from './error-handler.middleware'
 // export * from './request-id.middleware'
-// export * from './response-headers.middleware'
+export * from './response-headers.middleware'
 // export * from './response-mime.middleware'
