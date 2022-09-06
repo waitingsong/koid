44859b9c55ac76d6a54177ddc27f9e1663551424
waiting <1661926154@qq.com>
2022-09-06 11:54:33 +0800

    ci: koa

--- packages/midway-component-koid/src/configuration.ts
+++ packages/midway-component-koid/src/configuration.ts
@@ -3,6 +3,7 @@ import 'tsconfig-paths/register'
 import { join } from 'node:path'
 
 import { Configuration } from '@midwayjs/decorator'
+import * as koa from '@midwayjs/koa'
 
 import { ConfigKey } from './lib/types'
 
@@ -10,6 +11,7 @@ import { ConfigKey } from './lib/types'
 @Configuration({
   namespace: ConfigKey.namespace,
   importConfigs: [join(__dirname, 'config')],
+  imports: [koa],
 })
 export class AutoConfiguration {
 }
