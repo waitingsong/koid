f4fdcf6efc4a0fab8e9ff218e1a65ffc0faa5573
waiting <1661926154@qq.com>
2022-09-06 15:21:57 +0800

    ci: 9

--- packages/midway-component-koid/src/configuration.ts
+++ packages/midway-component-koid/src/configuration.ts
@@ -3,10 +3,15 @@ import 'tsconfig-paths/register'
 import { join } from 'node:path'
 
 import { ILifeCycle } from '@midwayjs/core'
-import { Configuration } from '@midwayjs/decorator'
+import { App, Configuration } from '@midwayjs/decorator'
+import { Application } from '@mwcp/share'
 
 import { useComponents } from './components'
 import { ConfigKey } from './lib/types'
+import {
+  ErrorHandlerMiddleware,
+  ResponseHeadersMiddleware,
+} from './middleware/index.middleware'
 
 
 @Configuration({
@@ -15,5 +20,27 @@ import { ConfigKey } from './lib/types'
   imports: useComponents,
 })
 export class AutoConfiguration implements ILifeCycle {
+  @App() readonly app: Application
+  async onReady(): Promise<void> {
+
+    // 定制化日志
+    // customLogger(this.logger, this.app)
+
+    // 全局x-request-id处理中间件
+    // @ts-ignore
+    // this.app.getMiddleware().insertFirst(RequestIdMiddleware)
+
+    // 全局错误处理中间件（确保在最前）
+    // @ts-ignore
+    this.app.getMiddleware().insertFirst(ErrorHandlerMiddleware)
+
+    const mws = [
+      // ResponseMimeMiddleware,
+      ResponseHeadersMiddleware,
+    ]
+    // @ts-ignore
+    this.app.useMiddleware(mws)
+
+  }
 }
 
