ffd47147a54e5125b29eb57115f76c525a6f257a
waiting <1661926154@qq.com>
2024-04-23 17:11:10 +0800

    ci(mwcp): change unit test port to 7002

--- packages/midway-component-koid/src/config/config.unittest.ts
+++ packages/midway-component-koid/src/config/config.unittest.ts
@@ -2,6 +2,9 @@ import { Config } from '##/lib/types.js'
 
 
 export const keys = 123456
+export const koa = {
+  port: 7002,
+}
 
 export const koidConfig: Partial<Config> = {
   enableDefaultRoute: true,
